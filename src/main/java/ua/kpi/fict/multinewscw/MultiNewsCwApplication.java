41596902303f534d3f49b772854442fd88738d2a
Anton Chaban <71810009+antonchaban@users.noreply.github.com>
2022-11-09 20:56:39 +0200

    Initial commit

---
+++ src/main/java/ua/kpi/fict/multinewscw/MultiNewsCwApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.fict.multinewscw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiNewsCwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiNewsCwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kpi/fict/multinewscw/MultiNewsCwApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.fict.multinewscw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiNewsCwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
