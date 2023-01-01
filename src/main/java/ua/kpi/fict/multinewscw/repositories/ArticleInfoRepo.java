dce7c8e2ab4b0b901a9ec5a5d46c073a73afdcd5
Anton Chaban <71810009+antonchaban@users.noreply.github.com>
2023-01-01 21:52:42 +0200

    removed

--- src/main/java/ua/kpi/fict/multinewscw/repositories/ArticleInfoRepo.java
+++
@@ -1,9 +0,0 @@
-package ua.kpi.fict.multinewscw.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ua.kpi.fict.multinewscw.entities.ArticleInfo;
-
-@Repository
-public interface ArticleInfoRepo extends JpaRepository<ArticleInfo, Long> {
-}
