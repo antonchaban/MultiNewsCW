6726b4e2fe5d50dc13a9868cc0952e56e3250907
Anton Chaban <71810009+antonchaban@users.noreply.github.com>
2023-01-05 19:07:39 +0200

    implemented elasticsearchClient

--- src/main/java/ua/kpi/fict/multinewscw/config/ElasticSearchConfig.java
+++ src/main/java/ua/kpi/fict/multinewscw/config/ElasticSearchConfig.java
@@ -1,8 +1,12 @@
 package ua.kpi.fict.multinewscw.config;
 
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
@@ -13,8 +17,13 @@ public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
     @Value("${elasticsearch.url}")
     public String elasticsearchUrl;
 
+    @Bean
     @Override
     public RestHighLevelClient elasticsearchClient() {
-        return null;
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(elasticsearchUrl)
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
     }
 }
