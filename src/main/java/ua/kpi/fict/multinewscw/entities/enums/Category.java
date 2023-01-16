adbc72c4295e378f3b033ff1a58411f2aaa4f60b
Anton Chaban <71810009+antonchaban@users.noreply.github.com>
2023-01-16 11:54:22 +0200

    created categories enum

---
+++ src/main/java/ua/kpi/fict/multinewscw/entities/enums/Category.java
@@ -0,0 +1,5 @@
+package ua.kpi.fict.multinewscw.entities.enums;
+
+public enum Category {
+    CATEGORY_WAR, CATEGORY_SPORT, CATEGORY_ECONOMY, CATEGORY_ENTERTAINMENT, CATEGORY_SCIENCE, CATEGORY_OTHER;
+}
